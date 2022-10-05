486ed1b6ff0cfea29d09be0f439bfb462390a5fe
Usuario <usuario@MacBook-Pro-de-ACT.local>
2022-10-04 23:03:53 -0300

    fix: heroku dialect

--- config/database.js
+++ config/database.js
@@ -4,5 +4,5 @@ module.exports = {
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
     dialect: process.env.DATABASE_DIALECT,
-    type: "postgres"
+    port: process.env.PORT,
 }
